706324a344c2da810bc005b57fbe41573ab6e253
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:39:22 +0530

    Add files via upload

---
+++ 5.java
@@ -0,0 +1,12 @@
+import Java.util.*;
+Public Covert{
+	Public static void main(string[]args){
+	 Scanner sc=new Scanner(System.in);
+	 System.out.print("Enter a floating point number:");
+	 double num= Sc.nextDouble();
+	 int converted= (int)num;
+	 System.out.println("original value: " + num);
+	 System.out.println("Converted integer value :" +converted);
+	Sc,close();
+	}
+}
