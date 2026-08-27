c8dbaec93a28eb346efdf71fc69850ccd5ebc7f3
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:39:07 +0530

    Add files via upload

---
+++ 4.java
@@ -0,0 +1,17 @@
+import Java.util.*;
+public relational{
+	public static void main (string[]args){
+	 ScannerSc = new Scanner(Syatem.in);
+	 System.out.println("Enter first number:"));
+	 int a =Sc.nextInt();
+	 System.out.print("Enter Second number:");
+	 int b =Sc.nextInt();
+	 System.out.print("a>b: " +(a>b));
+	 System.out.print("a<b: " +(a<b));
+	 System.out.print("a>=b: " +(a>=b));
+	 System.out.print("a<=b: " +(a<=b));
+	 System.out.print("a==b: " +(a==b));
+	 System.out.print("a!=b: " +(a!=b));
+	Sc.close();
+	}
+}
