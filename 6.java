e995095f9f552e5accdaa19de33c90cef0857483
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:39:37 +0530

    Add files via upload

---
+++ 6.java
@@ -0,0 +1,11 @@
+import Java.util.*;
+Public ASCIIValue{
+	public static void main(string[]args){
+	 Scanner Sc=new Scanner(system.in);
+	 System.out.print("Enter a character: ");
+	 char ch=Sc.next().charAt(0);
+	 int ascii==(int)ch;
+	 System.out.prinln("ASCII value of " + ch+" is: " + ascii);
+	 Sc.close()
+	}
+}
