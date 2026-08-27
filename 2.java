c94e7f1ab70857d990b1e97328b35a0bd0f9d4b8
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:38:25 +0530

    Add files via upload

---
+++ 2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+Public operations{
+	Public static void main9string[]args){
+	 System.out.println("Enter first number: ");
+	 int a=Sc.nextInt();
+	 System.out.println("Add:" + (a+b));
+	 System.out.println("sub:" + (a-b));
+	 System.out.println("Multiplication:" + (a*b));
+	 System.out.println("Division:" + (a/b));
+	 System.out.println("Modulus:" + (a%b));
+	 Sc.close();
+	}
+}
