bbb95d6a9604ac7d007cfbea752090c7dc72b4ed
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:40:25 +0530

    Add files via upload

---
+++ 9.java
@@ -0,0 +1,17 @@
+import Java.util.*;
+public Greatest{
+	public static void main(string[]args){
+	 Scanner.out.print("Enter first number:");
+	 int a =Sc.nextInt();
+	 System.out.print("Enter second number:");
+	 int b=Sc.nextInt();
+	 if(a>b){
+	 System.out.println(a + "is the greatest");
+	}else if (b>a){
+	 System.out.println(b + "is the greatest");
+	}else{
+	 System.out.println("Both no. are equal");
+	}
+	 Sc.close();
+	}
+}
