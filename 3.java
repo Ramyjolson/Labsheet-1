a82486d22b82c6e8881493c1e5067c838c9b648e
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:38:48 +0530

    Add files via upload

---
+++ 3.java
@@ -0,0 +1,14 @@
+import Java.util.*;
+Public EvenOdd{
+	public static void main(string[]args){
+	 ScannerSc=new Scanner(system.in);
+	 Scanner.out.print("Enter a number:");
+	 int num=Sc.nextInt();
+	 if(num%2==0){
+	 System.out.println("The number is Even:");
+	}else{
+	 System.out.println("The number is Odd:"):
+}
+	 Sc.close();
+	}
+}
