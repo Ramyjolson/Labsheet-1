bc5f732c4104b9638132977d045b4dff231763ab
Ramyjolson <k17ashutosh@gmail.com>
2026-08-27 11:38:06 +0530

    Add files via upload

---
+++ 1.java
@@ -0,0 +1,10 @@
+Public class Student{
+	Public Static Void main(String[]args){
+	 String name="Ashutosh";
+	 int age=20;
+	 String college="COER University";
+	 System.out.println("Name:"+ name);
+	 System.out.println("Age:" + age);
+	 System.out.println("college:" + college);
+	}
+}
